e65b23909dc68a734f0af41362d2f2234a9627a3
erozzae <rosihann14@mail.ugm.ac.id>
2025-02-25 19:15:28 +0700

    added nav tab on the portofolio section

--- src/layouts/PortofoliosLayout.jsx
+++ src/layouts/PortofoliosLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
 import PortofolioCard from "./../components/PortofoliosCard/PortofolioCard";
 import SubContentHeader from "../components/SubContentHeader/SubContentHeader";
 
@@ -9,28 +9,33 @@ const dummyData = [
       "https://www.jagoanhosting.com/blog/wp-content/uploads/2023/11/Website-Builder-Zyro.png",
     title: "Fullstack Music App",
     description: "Lorem, ipsum dolor.",
+    category: "Mini Project",
   },
   {
     image: "https://badoystudio.com/wp-content/uploads/2019/07/microsoft.png",
     title: "Fullstack Music App",
     description: "Lorem, ipsum dolor.",
+    category: "Real Project",
   },
   {
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDqwU7r8bwe1WXIskiipT9Gf0F2IG2smh8MAmB9_tss5e2yvK7aUUukyhlQlPRUkmttAo&usqp=CAU",
     title: "Fullstack Music App",
     description: "Lorem, ipsum dolor.",
+    category: "Real Project",
   },
   {
     image:
       "https://www.jagoanhosting.com/blog/wp-content/uploads/2023/11/Website-Builder-Zyro.png",
     title: "Fullstack Music App",
     description: "Lorem, ipsum dolor.",
+    category: "Real Project",
   },
   {
     image: "https://badoystudio.com/wp-content/uploads/2019/07/microsoft.png",
     title: "Fullstack Music App",
     description: "Lorem, ipsum dolor.",
+    category: "Real Project",
   },
 ];
 
@@ -43,18 +48,70 @@ const dummySubContentHeader = {
 function PortofoliosLayout() {
   return (
     <Container className="border border-primary shadow-sm mt-1">
-      <SubContentHeader subtitle={dummySubContentHeader.subtitle} description={dummySubContentHeader.description}/>
-      <Row>
-        {dummyData.map((item, index) => (
-          <Col className="mb-3" key={index} sm={12} md={6} lg={4}>
-            <PortofolioCard
-              image={item.image}
-              title={item.title}
-              description={item.description}
-            />
-          </Col>
-        ))}
-      </Row>
+      <SubContentHeader
+        subtitle={dummySubContentHeader.subtitle}
+        description={dummySubContentHeader.description}
+      />
+      <Tab.Container defaultActiveKey="all">
+        <Nav variant="tabs" className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link eventKey="all">Semua Project</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="real">Real Project</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="mini">Mini Project</Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <Tab.Content className="mt-3">
+          <Tab.Pane eventKey="all">
+            <Row>
+              {dummyData.map((item, index) => (
+                <Col className="mb-3" key={index} sm={12} md={6} lg={4}>
+                  <PortofolioCard
+                    image={item.image}
+                    title={item.title}
+                    description={item.description}
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Tab.Pane>
+          <Tab.Pane eventKey="real">
+            <Row>
+              {dummyData
+                .filter((item) => item.category === "Real Project") // Filter data
+                .map((
+                  item,
+                  index // Gunakan map untuk merender elemen
+                ) => (
+                  <Col className="mb-3" key={index} sm={12} md={6} lg={4}>
+                    <PortofolioCard
+                      image={item.image}
+                      title={item.title}
+                      description={item.description}
+                    />
+                  </Col>
+                ))}
+            </Row>
+          </Tab.Pane>
+          <Tab.Pane eventKey="mini">
+            {dummyData
+              .filter((item) => item.category === "Mini Project")
+              .map((item, index) => (
+                <Col className="mb-3" key={index} sm={12} md={6} lg={4}>
+                  <PortofolioCard
+                    image={item.image}
+                    title={item.title}
+                    description={item.description}
+                  />
+                </Col>
+              ))}
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
     </Container>
   );
 }
