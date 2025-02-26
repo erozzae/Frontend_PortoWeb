16447466ef3ae09b04cb48380a86268462f52220
erozzae <rosihann14@mail.ugm.ac.id>
2025-02-26 13:28:21 +0700

    have revised responsive for message form

--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -6,13 +6,13 @@ function MessageForm() {
     <>
       <Form>
         <Row>
-          <Col>
+          <Col xs={12} lg={6}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="name@example.com" />
             </Form.Group>
           </Col>
-          <Col>
+          <Col xs={12} lg={6}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="name@example.com" />
