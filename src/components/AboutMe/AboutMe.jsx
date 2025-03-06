5bb6374117d2708b28ffb2956a0294280deef8f6
erozzae <rosihann14@mail.ugm.ac.id>
2025-03-06 15:53:07 +0700

    have revisied about me comp

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,35 +1,32 @@
 import React from "react";
+import styles from "../../styles/global.module.css";
 import { Col, Row, Button, Image, Container } from "react-bootstrap";
 
 function AboutMe() {
   return (
-    <Container style={{ marginTop:"10rem" }}>
+    <Container className="" style={{ marginTop: "10rem" }}>
       <Row>
-        <Col className="mb-3" sm={12} md={6}>
-          <h1>Hi! Iam Rosyihan Fullstack Developer</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis
-            saepe molestiae a odit beatae fugiat excepturi ea, fugit adipisci
-            soluta vitae quae, harum consectetur rem fuga expedita officiis
-            quisquam quam libero blanditiis atque ipsa natus recusandae. Totam,
-            quam cupiditate?
-          </p>
-          <div className="btn-socmed d-flex">
-            <Button variant="primary">Download CV</Button>
+        <Col className="mb-3" sm={12} md={8}>
+          <h1 style={{ fontSize:"4rem" }} className={`fw-bold`}>Hi! I am <span className={styles.spanTextCustomGradienAnim}>Rosyihan</span>, a <span >Fullstack Developer.</span></h1>
+          <p>Turning digital ideas into powerful and helpful</p>
+          <div className="btn-socmed d-flex text-btn">
+            <Button className={`${styles.btnCustomPrimary}`}>
+              Download CV
+            </Button>
             <div className="socmed d-flex align-items-center">
-              <div className="border border-primary ms-3 p-1 rounded">
+              <div className={`${styles.iconBorderPrimary} ms-3 p-1 rounded`}>
                 <a href="#" className="">
                   <i className="bx bxl-linkedin"></i>
                 </a>
               </div>
-              <div className="border border-primary ms-3 p-1 rounded">
+              <div className={`${styles.iconBorderPrimary} ms-3 p-1 rounded`}>
                 <a href="#" className="">
                   <i className="bx bxl-github"></i>
                 </a>
               </div>
-              <div className="border border-primary ms-3 p-1 rounded">
+              <div className={`${styles.iconBorderPrimary} ms-3 p-1 rounded`}>
                 <a href="#" className="">
-                <i className='bx bxl-gmail'></i>
+                  <i className="bx bxl-gmail"></i>
                 </a>
               </div>
             </div>
@@ -37,9 +34,9 @@ function AboutMe() {
         </Col>
 
         <Col
-          className="d-flex justify-content-center align-items-center mb-3" 
+          className="d-flex justify-content-center align-items-center mb-3"
           sm={12}
-          md={6}
+          md={4}
         >
           <Image
             src="https://images.immediate.co.uk/production/volatile/sites/3/2024/05/How-many-Pokemon-are-there-6434211.jpg?quality=90&resize=980,654"
